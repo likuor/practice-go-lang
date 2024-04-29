089eaeba1865d355655820ec35ddc3aab56fb445
likuor <likuor0528@gmail.com>
2024-04-28 23:06:28 -0700

    add: function

--- greeting.go
+++ greeting.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+func sayHello (text string) {
+	fmt.Println(text)
+}
+
+func cal (x, y int) (res int) {
+	res = x + y
+	return
+}
+
+func calc2 (x, y int) (a int,b int) {
+	a = x * y
+	b = x + y
+	return
+}
+
 func main() {
 // x:=12
 // y:=12
@@ -18,25 +33,34 @@ func main() {
 	// 	fmt.Println("aaaaaaa")
 	// }
 
-	i:=0
-	for i <= 10 {
-		fmt.Println("loop 1", i)
-		i++
-	}
+	// i:=0
+	// for i <= 10 {
+	// 	fmt.Println("loop 1", i)
+	// 	i++
+	// }
 
-	x := 0
-	for  {
-		fmt.Println("loop 2", x)
-		if x == 10 {
-			break
-		}
-		x++
-	}
+	// x := 0
+	// for  {
+	// 	fmt.Println("loop 2", x)
+	// 	if x == 10 {
+	// 		break
+	// 	}
+	// 	x++
+	// }
 
-	for i := 0; i <= 10; i++ {
-		fmt.Println("loop 3", i)
-	}
+	// for i := 0; i <= 10; i++ {
+	// 	fmt.Println("loop 3", i)
+	// }
 
+	func (greeting string) {
+		fmt.Println(greeting)
+	}("Evening")
+
+	sayHello("Morning")
+	res1, res2 := calc2(10, 2)
+	fmt.Println(res1, res2)
+
+	fmt.Println(cal(10, 5))
 }
 
 
